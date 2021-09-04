6c02739ee03fbc152253f8e83b27c2a2d180b1b0
michimichi2021 <michikaho1009@gmail.com>
2021-09-04 12:52:11 +0000

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,48 @@
 Rails.application.routes.draw do
+
+
   devise_for :admins,controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
   registrations: 'admins/registrations'
-}
+ }
+
 
   devise_for :users,controllers: {
   sessions:      'users/sessions',
   passwords:     'users/passwords',
   registrations: 'users/registrations'
-}
-  root 'homes#top'
-  
-  
+ }
+
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update]
+
+  end
+
+
+  root to: "homes#top"
+
+
+
+  scope module: :public do
+
+
+    resource :users, only: [:edit, :update] do
+     get '/my_page' => 'users#show'
+     get '/unsubscribe' => 'users#unsubscribe'
+     get '/withdraw' => 'users#withdraw'
+    end
+
+    resources :items, only:[:show, :new, :create, :edit, :update] 
+
+    resources :purchases, only:[:new, :create, :index, :show] do
+     get '/confirm' => 'purchases#confirm'
+     get '/complete' => 'purchases#complete'  
+    end
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
