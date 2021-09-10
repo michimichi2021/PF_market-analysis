a172add259f12341e9e66b3ee59b9d1561e46ea7
michimichi2021 <michikaho1009@gmail.com>
2021-09-10 15:32:20 +0000

    分析データのページに購入された商品を表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -31,7 +31,13 @@ class Item < ApplicationRecord
   def self.search(word)
    @items = Item.where("name LIKE?","%#{word}%")
   end
-
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
+  
+  
 
   has_one_attached :image
 
@@ -45,8 +51,6 @@ class Item < ApplicationRecord
       errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
     end
   end
-
-
 
 
 end
