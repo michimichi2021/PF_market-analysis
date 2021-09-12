5589a163b11560d39320a40ea6b439602c4ccd5c
michimichi2021 <michikaho1009@gmail.com>
2021-09-12 10:32:24 +0000

    非同期通信

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,23 +1,28 @@
 class Public::CommentsController < ApplicationController
   
-  def create
-    item = Item.find(params[:item_id])
-    comment = Comment.new(comment_params)
-    comment.user_id = current_user.id
-    comment.item_id = item.id
-    comment.save
-    redirect_to item_path(item)
-  end
-  
-  def destroy
-    Comment.find_by(id: params[:id], item_id: params[:item_id]).destroy
-    redirect_to item_path(params[:item_id])
-  end
-  
-  private
+def create
+@item = Item.find(params[:item_id])
+@comment = Comment.new(comment_params)
+@comment.user_id = current_user.id
+@comment.item_id = @item.id
+@comments = @item.comments
+@comment.save
+respond_to do |format|
+format.js
+end
+end
 
-  def comment_params
-    params.require(:comment).permit(:comment)
-  end
-  
+def destroy
+  @item = Item.find(params[:item_id])
+  @comments = @item.comments
+  @comment = Comment.find_by(id: params[:id], item_id: params[:item_id])
+  @comment.destroy
+end
+
+private
+
+def comment_params
+  params.require(:comment).permit(:comment, :item_id, :user_id)
+end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -29,7 +29,7 @@ class Item < ApplicationRecord
   end
 
   def self.search(word)
-   @items = Item.where("name LIKE?","%#{word}%")
+   where("name LIKE?","%#{word}%")
   end
   
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
@@ -37,7 +37,7 @@ class Item < ApplicationRecord
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
   
-  
+  scope :purchased,-> { where(is_active: false) } 
 
   has_one_attached :image
 
