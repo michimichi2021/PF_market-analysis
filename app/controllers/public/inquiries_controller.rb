28419aada26e7017429b9048aa7d5548b3fc2d07
michimichi2021 <michikaho1009@gmail.com>
2021-09-13 05:03:23 +0000

    お問い合わせエラー

---
+++ app/controllers/public/inquiries_controller.rb
@@ -0,0 +1,20 @@
+class Public::InquiriesController < ApplicationController
+  def index
+    @inquiry = Inquiry.new
+  end
+
+  def confirm
+    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))
+    if @inquiry.valid?
+      render 'confirm'
+    else
+      render 'index'
+    end
+  end
+
+  def thanks
+    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))    
+    InquiryMailer.received_email(@inquiry).deliver
+    render 'thanks'
+  end
+end
