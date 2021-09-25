f78bd1ce68d885fbce5846f86915c34a450a1c62
michimichi2021 <michikaho1009@gmail.com>
2021-09-25 06:35:07 +0000

    relationshipモデルのテスト記述完了

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe 'relationshipモデルのテスト', type: :model do
   describe 'アソシエーションのテスト' do
     context 'userモデルとの関係' do
       it 'N:1となっている' do
-        expect(Relationship.reflect_on_association(:user).macro).to eq :belongs_to
+        expect(Relationship.reflect_on_association(:follower).macro).to eq :belongs_to
+      end
+    end
+    context 'userモデルとの関係' do
+    it 'N:1となっている' do
+        expect(Relationship.reflect_on_association(:followed).macro).to eq :belongs_to
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe 'userモデルのテスト', type: :model do
     @user.image = fixture_file_upload('app/assets/images/no_image.jpeg')
   end
 
+  describe "フォローフォロワー" do
+
+    it "フォロー" do
+      @other_user = FactoryBot.create(:user, image:fixture_file_upload('app/assets/images/no_image.jpeg'))
+      @another_user = FactoryBot.create(:user, image:fixture_file_upload('app/assets/images/no_image.jpeg'))
+      Relationship.create(follower_id:@other_user.id, followed_id:@another_user.id)
+      expect(@other_user.following_user.map(&:id)).to include @another_user.id
+    end
+  end
+
   context "バリデーションチェック" do
     it "有効な登録内容の場合は保存されるか" do
       expect(@user).to be_valid
